ab727c7f302dca5682386f376d828b17e486b24c
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-06-12 22:28:25 -0300

    Added initial cards list

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DeadLines',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'DeadLines'),
     );
   }
 }
@@ -36,25 +36,66 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  List<Widget> _createCards(BuildContext context) {
+    return new List<Widget>.generate(_counter, (index) =>
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.album),
+                title: Text('Thing'),
+                subtitle: Text('Due in two days'),
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('FINISHED'),
+                    onPressed: () {
+                      _decrementCounter();
+
+                      final snackBar = SnackBar(
+                        content: Text('Nice!'),
+                        action: SnackBarAction(
+                          label: 'Undo',
+                          onPressed: () {
+                            _incrementCounter();
+                          },
+                        ),
+                      );
+                      Scaffold.of(context).showSnackBar(snackBar);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Builder(
+        builder: (context) =>
+            Align(
+              alignment: Alignment.topCenter,
+              child: ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(15.0),
+                  children: _createCards(context)
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
