51595a197727c1136126fd710944dcee480a7d6b
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-06-18 16:03:14 -0300

    Added test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:deadlinesapp/db/dao/deadline_dao.dart';
+import 'package:deadlinesapp/db/database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:deadlinesapp/main.dart';
 
 void main() {
+  AppDatabase database;
+  DeadlineDao deadlineDao;
+
+  setUp(() async {
+    database = await $FloorAppDatabase
+        .inMemoryDatabaseBuilder()
+        .build();
+    deadlineDao = database.deadlineDao;
+  });
+
+  tearDown(() async {
+    await database.close();
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(DeadlinesApp(deadlineDao));
+
+    expect(find.byElementType(Card), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.byElementType(Card), findsOneWidget);
   });
 }
