11b475187a72ce470bc01f6699d85e21237c1512
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-10-13 23:39:30 -0300

    Added Android flavors

--- lib/flavorconf/flavors.dart
+++ lib/flavorconf/flavors.dart
@@ -1,12 +1,15 @@
+const int FLAVOR_DEBUG = -1;
 const int FLAVOR_FREE = 0;
 const int FLAVOR_PAID = 1;
 const int FLAVOR_FDROID = FLAVOR_FREE;
 
-const int FLAVOR = FLAVOR_FREE;
+const int FLAVOR = FLAVOR_DEBUG;
 
 class FlavorConfig {
   static String appName() {
     switch(FLAVOR) {
+      case FLAVOR_DEBUG:
+        return  "DeadlinesDebug";
       case FLAVOR_FREE:
         return  "DeadlinesFree";
       case FLAVOR_PAID:
