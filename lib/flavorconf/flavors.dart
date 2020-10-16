edd534fc7dd6b5d3176abea6c27a25b3edd06a8d
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-10-16 11:03:50 -0300

    Set shareduserid, target API 29, flavors into commandline

--- lib/flavorconf/flavors.dart
+++ lib/flavorconf/flavors.dart
@@ -3,7 +3,22 @@ const int FLAVOR_FREE = 0;
 const int FLAVOR_PAID = 1;
 const int FLAVOR_FDROID = FLAVOR_FREE;
 
-const int FLAVOR = FLAVOR_FDROID;
+final int FLAVOR = _flavor();
+
+int _flavor() {
+  switch(const String.fromEnvironment('FLAVOR', defaultValue: null)) {
+    case "FREE":
+      return FLAVOR_FREE;
+    case "DEBUG":
+      return FLAVOR_DEBUG;
+    case "PAID":
+      return FLAVOR_PAID;
+    case "FDROID":
+      return FLAVOR_FDROID;
+    default:
+      return null;
+  }
+}
 
 class FlavorConfig {
   static String appName() {
